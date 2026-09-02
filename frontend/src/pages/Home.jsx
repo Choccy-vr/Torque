d9502a9048fe6340f38aa24f9c6090196194874e
MustafaBioS <mustaafahany@gmail.com>
2026-09-02 09:13:58 +0300

    feat: text and layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,35 @@ import flag from '../assets/flag-orpheus-top.png'
 
 
 export default function Home() {
-    return (
-        <>
-            <div className="fixed inset-0 -z-10 bg-[#070707] bg-[linear-gradient(#ffffff10_1px,transparent_1px),linear-gradient(90deg,#ffffff10_2px,transparent_2px)] bg-size-[50px_50px]"></div>
-            <img src={flag} alt="Flag" className="fixed top-0 left-6 h-20" />
-            <button className="fixed top-8 right-10 bg-white px-12 py-2 text-black rounded-md hover:opacity-80 transition-all duration-300 ease">Log In</button>
-        </>
-    )
+  return (
+    <>
+      <div className="fixed inset-0 z-0 bg-[#d48300] [background-image:linear-gradient(#f7ff0040_1px,transparent_1px),linear-gradient(90deg,#f7ff0040_1px,transparent_1px)] [background-size:50px_50px]" />
+
+      <div>
+        <img
+          src={flag}
+          alt="Hack Club Flag"
+          className="fixed top-0 left-6 z-10 h-20 hover:opacity-75 cursor-pointer transition-all duration-300 ease"
+          onClick={() => window.open('https://hackclub.com', '_blank')}
+        />
+
+        <button className="fixed top-8 right-10 z-10 rounded-md bg-white px-12 py-2 text-black transition-all duration-300 ease hover:opacity-80">
+          Log In
+        </button>
+      </div>
+
+      <div className="absolute top-0 left-0 right-0 flex items-center justify-center min-h-screen z-50" data-landing-target="heroContent">
+        <div className="w-full max-w-6xl mx-auto px-6 md:px-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 items-center">
+            <div className="space-y-2 md:space-y-4 text-center md:text-left">
+              <h1 className="text-4xl md:text-5xl font-phantom font-bold text-white">Torque</h1>
+              <p className="font-phantom text-2xl text-white">Build hardware projects with motors.<br/> Get funding and prizes.</p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </>
+  )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,7 @@ export default {
   ],
   theme: {
     extend: {},
+    phantom: ["Phantom Sans", 'sans-serif'],
   },
   plugins: [],
 }
