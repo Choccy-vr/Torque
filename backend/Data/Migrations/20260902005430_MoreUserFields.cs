cfbc17d9a44ad9f3109f6b5ff5d9abf0239f22f7
Ginobeano <94711504+Choccy-vr@users.noreply.github.com>
2026-09-01 20:55:25 -0400

    feat: add more user fields

---
+++ backend/Data/Migrations/20260902005430_MoreUserFields.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace torque_backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoreUserFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid[]>(
+                name: "devlogs",
+                table: "users",
+                type: "uuid[]",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "hackatime_id",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "hc_user_id",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "profile_picture_url",
+                table: "users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid[]>(
+                name: "projects",
+                table: "users",
+                type: "uuid[]",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "role",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "slack_user_id",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "total_devlogs",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "total_projects",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "total_time_shipped",
+                table: "users",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "verification_status",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "volts",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ysws_eligible",
+                table: "users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "devlogs",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "hackatime_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "hc_user_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "profile_picture_url",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "projects",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "role",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "slack_user_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "total_devlogs",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "total_projects",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "total_time_shipped",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "verification_status",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "volts",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "ysws_eligible",
+                table: "users");
+        }
+    }
+}
--- backend/modules/users/User.cs
+++ backend/modules/users/User.cs
@@ -7,7 +7,20 @@ public class User
     public string Username { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Bio { get; set; } = null!;
-
+    public string Name { get; set; } = null!;
+    public string? ProfilePictureUrl { get; set; }
+    public int Volts { get; set; }
+    public float TotalTimeShipped { get; set; }
+    public int TotalProjects { get; set; }
+    public int TotalDevlogs { get; set; }
+    public Guid[]? Projects { get; set; }
+    public Guid[]? Devlogs { get; set; }
+    public string SlackUserID { get; set; } = null!;
+    public string Role { get; set; } = null!;
+    public string HcUserID { get; set; } = null!;
+    public string HackatimeID { get; set; } = null!;
+    public bool YswsEligible { get; set; } = false;
+    public bool VerificationStatus { get; set; } = false;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
 }
